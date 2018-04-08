405d1411a5988985d35cbf2c7f395ef1c747c7d8
Christopher Smith <christophersmith@users.noreply.github.com>
2018-04-08 14:52:12 -0400

    updated version of Spring Framework. updated license file in Java classes.

--- summer-mqtt-core/src/test/java/com/github/christophersmith/summer/mqtt/core/event/MqttClientConnectionFailureEventTest.java
+++ summer-mqtt-core/src/test/java/com/github/christophersmith/summer/mqtt/core/event/MqttClientConnectionFailureEventTest.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2017 Christopher Smith
+ * Copyright (c) 2018 Christopher Smith - https://github.com/christophersmith
  * 
  * Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except
  * in compliance with the License. You may obtain a copy of the License at
